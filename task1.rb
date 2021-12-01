745755523ee38e142d9ec2a677974da8757c8a39
VikaZinovieva <86409634+VikaZinovieva@users.noreply.github.com>
2021-12-01 14:46:47 +0200

    Update task1.rb

--- task1.rb
+++ task1.rb
@@ -3,24 +3,22 @@ class DataBase
   @@users = {}
   @@users_count = 0
 
-  def print_global
-    puts "Global variable is #$storage"
-  end
+ def print_global
+  puts "Global variable is #$storage"
+ end
 
-  def initialize(username, password)
-    @username = username
-    @password = password
-    @@users[username] = password
-    @@users_count += 1
-  end
+ def initialize(username, password)
+  @username = username
+  @password = password
+  @@users[username] = password
+  @@users_count += 1
+ end
 
-  def my_credentials
-    puts "I'm #{@username} and my pass is #{@password}"
-  end
+ def my_credentials
+  puts "I'm #{@username} and my pass is #{@password}"
+ end
 
-    def how_many
-      puts "There are users: #{@@users_count}"
-    end
+ def how_many
+  puts "There are users: #{@@users_count}"
+ end
 end
-
-
